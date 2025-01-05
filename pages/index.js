2647d24e4e183f47f0b69f01f07a780157b50359
Ghulam Muhammad Ahmad <157963398+Ghulam-Muhammad-Ahmad@users.noreply.github.com>
2025-01-06 00:52:56 +0500

    add title

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,17 @@
 "use client"
 import Header from '@/components/Header';
 import LandingPage from '@/components/LandingPage';
+import { useEffect } from 'react';
 
 export default function Home() {
+  useEffect(() => {
+    document.title = "ChillBill - Your Personal Finance Manager";
+  }, []);
+
   return (
   <>
-<Header />
-<LandingPage />
+    <Header />
+    <LandingPage />
   </>
   );
 }
