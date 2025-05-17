bd2c11cdcec2f96f570e9ed8d6da5c2e63164b58
Ghulam Muhammad Ahmad <157963398+Ghulam-Muhammad-Ahmad@users.noreply.github.com>
2025-05-18 00:03:19 +0500

    Add Vercel Insights

--- pages/_app.js
+++ pages/_app.js
@@ -3,12 +3,14 @@ import { SessionProvider } from 'next-auth/react'; // Import SessionProvider
 import '../styles/globals.css'; // Your global styles
 import '../styles/marquee.css'
 import { MonthProvider } from '@/context/monthContext';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 function MyApp({ Component, pageProps }) {
   return (
     <SessionProvider session={pageProps.session}>
       <MonthProvider> {/* Wrap your app here */}
         <Component {...pageProps} />
+        <SpeedInsights />
       </MonthProvider>
     </SessionProvider>
   );
